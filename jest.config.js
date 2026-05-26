47f27d919ce38932e32b34887419b5066ae05bf7
TonyQ <tonylovejava@gmail.com>
2026-05-26 11:11:38 +0800

    chore: update app source metadata

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  modulePathIgnorePatterns: ['<rootDir>/.bat-worktrees/'],
+  testPathIgnorePatterns: ['<rootDir>/.bat-worktrees/'],
 };
